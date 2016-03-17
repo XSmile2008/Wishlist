d33a08a81f56d64b5970057cb5b7023db4d0ce1b
odahovskiy <odahovskiy@gmail.com>
2016-03-17 21:58:34 +0200

    Add indexed hash set

--- app/src/main/java/com/company/wishlist/adapter/TopWishAdapter.java
+++ app/src/main/java/com/company/wishlist/adapter/TopWishAdapter.java
@@ -9,18 +9,15 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.company.wishlist.R;
 import com.company.wishlist.activity.TopWishActivity;
 import com.company.wishlist.activity.WishEditActivity;
 import com.company.wishlist.fragment.WishListFragment;
 import com.company.wishlist.model.Wish;
 import com.company.wishlist.util.CloudinaryUtil;
-import com.company.wishlist.util.CropCircleTransformation;
+import com.company.wishlist.component.IndexedHashSet;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -30,17 +27,17 @@ import butterknife.OnClick;
 public class TopWishAdapter extends RecyclerView.Adapter<TopWishAdapter.Holder> {
 
     private Context context;
-    private Set<Wish> wishes;
+    private IndexedHashSet<Wish> wishes;
     private String wishListId;
 
     public TopWishAdapter(Context context, String wishListId) {
         this.context = context;
-        this.wishes = new HashSet<>();
+        this.wishes = new IndexedHashSet<>();
         this.wishListId = wishListId;
     }
 
     private Wish getByIndex(int index) {
-        return (Wish) wishes.toArray()[index];//TODO: optimize this
+        return wishes.get(index);//TODO: optimize this
     }
 
     @Override

4205b33edcf853bee3550975a2264ae35a8642f3
XSmile2008 <XSmile2008@gmail.com>
2016-03-18 02:25:11 +0200

    replace screenshots with icons, various fixes

--- app/src/main/java/com/company/wishlist/activity/IntroActivity.java
+++ app/src/main/java/com/company/wishlist/activity/IntroActivity.java
@@ -14,30 +14,32 @@ public class IntroActivity extends AppIntro {
 
     @Override
     public void init(Bundle savedInstanceState) {
+
+        //Title slide
         addSlide(AppIntroFragment.newInstance(getString(R.string.first_intro_title), getString(R.string.first_intro_content),
                 R.drawable.circle_gift, Color.parseColor("#2196F3")));
 
-        addSlide(AppIntroFragment.newInstance("Create wishes", Html.fromHtml("Create new wish that allow friends to know your dreams and don't keep it in your mind."),
-                R.drawable.own_wish, Color.parseColor("#2196F3")));
+        addSlide(AppIntroFragment.newInstance("Keep your own Wish List", "Create your own wishlist, " +
+                        "fill it with all your wisher, and let your friends know what you wish.",
+                R.drawable.tablet, Color.parseColor("#3E2723")));
 
-        addSlide(AppIntroFragment.newInstance("Keep your own Wish List", Html.fromHtml("Wish list view your wishes that separated by sections, " +
-                        "that will help to navigate through list."),
-                R.drawable.your_wish, Color.parseColor("#2196F3")));
+        addSlide(AppIntroFragment.newInstance("Friends", "Discover your friends wishlists. \n" +
+                        "You can watch what your friends wish, and realize their dreams",
+                R.drawable.user, Color.parseColor("#0D47A1")));
 
-        addSlide(AppIntroFragment.newInstance("Navigation drawer", Html.fromHtml("This panel view your profile data and friend list. </br> " +
-                        "After selection of friend you will see friend wishes and your own list for this friend."),
-                R.drawable.friends, Color.parseColor("#2196F3")));
+        addSlide(AppIntroFragment.newInstance("Create wishes", "Create new wish that allow friends to know your dreams and don't keep it in your mind.",
+                R.drawable.notes, Color.parseColor("#F57F17")));
 
-        addSlide(AppIntroFragment.newInstance("Friend wish list", Html.fromHtml("Watching for actual friend dreams and reserve it to give. "),
-                R.drawable.friend_wish, Color.parseColor("#2196F3")));
+        addSlide(AppIntroFragment.newInstance("Add images", "You can choose image for you wish from your phone or use image search",
+                R.drawable.images, Color.parseColor("#33691E")));
 
-        addSlide(AppIntroFragment.newInstance("Gift list", Html.fromHtml("Bring own wish list for friend."),
-                R.drawable.gift_list, Color.parseColor("#2196F3")));
+        addSlide(AppIntroFragment.newInstance("Reservation", "If you want make some gift from wishlist reserve it and another users will be seen that this wish will be fulfilled by you",
+                R.drawable.love, Color.parseColor("#B71C1C")));
 
-        setBarColor(Color.parseColor("#303F9F"));
-        setSeparatorColor(Color.parseColor("#3F51B5"));
+        addSlide(AppIntroFragment.newInstance("Notifications", "After you reserve some wish app notify you buy a gift",
+                R.drawable.count, Color.parseColor("#263238")));
+
         showSkipButton(true);
-
         setVibrate(true);
         setVibrateIntensity(30);
     }
